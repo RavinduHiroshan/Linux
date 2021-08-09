ad2456d5c9c15714cbda0e62aea13f4651ac69d6
Ravindu Hiroshan <58454450+RavinduHiroshan@users.noreply.github.com>
2021-08-10 01:27:42 +0530

    Delete OpenLDAP directory

--- OpenLDAP/encrypt.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-userInput=str(input())
-noise=bytes("Km5d5ivMy8iexuHcZrsD","ascii")
-epoh=200000
-conver = bytes(userInput,"ascii")
-output = hashlib.pbkdf2_hmac('sha512', conver, noise , epoh)
-print(output.hex())
-
